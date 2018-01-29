acff484e84e0dd7522c4f697e5b6c662a9449f84
Omar Abdelwahab <Omieabdelwahab@gmail.com>
2018-01-29 13:14:58 -0700

    Opened File and error checking for opening it

--- src/assignment/java/main/fileIO.java
+++ src/assignment/java/main/fileIO.java
@@ -8,14 +8,18 @@ public class fileIO {
 	//this is a preattempt at opening a file
 	//final version should return a buffered reader or scanner (havn't decided yet)
 	//I'll make another function below that parses it and adds it to their respective places
-	private static void getFileInfo() throws IOException {
-		FileReader in = null;
-		
+	private static void getFileInfo(String fileName) throws IOException {
+		FileInputStream Fin = null;
 		try {
-			
+			Fin = new FileInputStream(fileName);
+			int c;
+			while ((c = Fin.read()) != -1) {
+				
+				
+			}
 		}finally{
-			if(in != null) {
-				in.close();
+			if(Fin != null) {
+				Fin.close();
 			}
 		}
 	}
