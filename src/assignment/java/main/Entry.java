89778347d55992a21726f9e5a2e82bb337ee98ef
Unknown <Omieabdelwahab@gmail.com>
2018-02-08 19:36:01 -0700

    added subscripts to final output for working results

--- src/assignment/java/main/Entry.java
+++ src/assignment/java/main/Entry.java
@@ -1,4 +1,4 @@
-//package assignment.java.main;
+package assignment.java.main;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -14,7 +14,7 @@ public class Entry {
         }
         try {
             PrintWriter writer = new PrintWriter(args[1]);
-            Constraints c = fileIO.fileIO(args[0], writer);
+            Constraints c = fileIO.fileIO(args[0]);
             State state = new State();
             PossibilityTree pt = new PossibilityTree(
                     new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'}, c);
@@ -27,9 +27,9 @@ public class Entry {
             }
 
             else {
-                returned = "Solution " + min.entries[0] + min.entries[1]
-                        + min.entries[2] + min.entries[3] + min.entries[4]
-                        + min.entries[5] + min.entries[6] + min.entries[7]
+                returned = "Solution " + min.entries[0] + "\u2081" + min.entries[1] + "\u2082"
+                        + min.entries[2] + "\u2083" + min.entries[3] + "\u2084" + min.entries[4] + "\u2085"
+                        + min.entries[5] + "\u2086" + min.entries[6] + "\u2087" + min.entries[7] + "\u2088"
                         + "; Quality: " + min.penalty;
             }
             writer.println(returned);
