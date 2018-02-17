3d6632fbae479d092d7df62ea5ac7d0eee2c7670
Anastasiya Lazarenko <laz1anastasiya@gmail.com>
2018-02-17 15:12:31 -0700

    bash scripts for making jar files

---
+++ src/jars/make_nopckg_jar.sh
@@ -0,0 +1,3 @@
+cd /home/anastasiya/git/449-w18-T2-Java/bin/assignment/java/main
+jar cvfm newnewnopckgjar.jar /home/anastasiya/git/449-w18-T2-Java/bin/jars/mani_inf.txt * /home/anastasiya/git/449-w18-T2-Java/src/assignment/java /home/anastasiya/git/449-w18-T2-Java/README.md
+
---
+++ src/jars/make_pckg_jar.sh
@@ -0,0 +1,3 @@
+cd /home/anastasiya/git/449-w18-T2-Java/bin
+jar cvfm newnewpckgjar.jar /home/anastasiya/git/449-w18-T2-Java/bin/jars/mani_inf.txt assignment/java/main /home/anastasiya/git/449-w18-T2-Java/src/assignment/java /home/anastasiya/git/449-w18-T2-Java/README.md
+
